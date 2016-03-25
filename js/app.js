718f756d8b7ec2858981e3e0159fedec54165014
dawsonjones808 <dawsonjones@student.midpac.edu>
2016-03-25 11:23:05 -1000

    Finished second upgrade
    iron chef grandma
    
    3000 brownies for 3 brownies per click

--- js/app.js
+++ js/app.js
@@ -1,22 +1,41 @@
 var count = 0;
 var bonus = 0;
-
+var bonusPoint = 0;
+var bonusmultiplier = 0;
 
 var press = function() {
 	count++;
 	count=count+bonus;
 	document.getElementById('score').innerHTML = "Current Brownies: " + count;
 	console.log("press variable");
+	if (Math.floor(Math.random() * 100) == 1){
+		count= count+200
+		document.getElementById('score').innerHTML = "Current Brownies: " + count;
+		console.log("You got a Corner Brownie!")
+	}
 }
+
 var grandma = function() {
 	if (count>=2000){
-	bonus=1;
-	count= count-2000;
+		bonus=1;
+		count= count-2000;
+		document.getElementById('score').innerHTML = "Current Brownies: " + count;
+	}
+	else{
+		bonus=0;
+	}
+} 
+
+var ironchef = function() {
+	if (count>=3000){
+		bonus=2;
+	count= count-3000;
 	document.getElementById('score').innerHTML = "Current Brownies: " + count;
 	}
 	else{
 		bonus=0;
 	}
-
-	console.log("click variable");
 }
+
+
+console.log("click variable");
