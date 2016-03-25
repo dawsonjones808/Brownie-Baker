31dd011ecc3a55c8691a8b00f974c18d6307a0da
dawsonjones808 <dawsonjones@student.midpac.edu>
2016-03-24 16:35:36 -1000

    Brownie Baker
    We got the grandma to work better

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var count = 0;
 var bonus = 0;
 
+
 var press = function() {
 	count++;
 	count=count+bonus;
@@ -8,6 +9,14 @@ var press = function() {
 	console.log("press variable");
 }
 var grandma = function() {
-	bonus=1;
+	if (count>=2000){
+	bonus=3;
+	count= count-2000;
+	document.getElementById('score').innerHTML = "Current Brownies: " + count;
+	}
+	else{
+		bonus=0;
+	}
+	
 	console.log("click variable");
 }
