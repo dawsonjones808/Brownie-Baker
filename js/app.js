d0f195a2d55eb1c00e80be86e2ec4fd2f82dedd3
dawsonjones808 <dawsonjones@student.midpac.edu>
2016-03-24 16:01:58 -1000

    Finished the grandma
    we got the grandma button to work properly this time

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 var count = 0;
+var bonus = 0;
+
 var press = function() {
 	count++;
+	count=count+bonus;
 	document.getElementById('score').innerHTML = "Current Brownies: " + count;
+	console.log("press variable");
+}
+var grandma = function() {
+	bonus=1;
+	console.log("click variable");
 }
